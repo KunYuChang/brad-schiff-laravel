3e5b6bb4d8617d4d282471f94aab550a8774cd36
KunYuChang <mischiefsub@gmail.com>
2024-02-18 19:53:11 +0800

    設置基本的 routes, controller, view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class, 'homepage']);
+Route::get('/about', [ExampleController::class, 'about']);
