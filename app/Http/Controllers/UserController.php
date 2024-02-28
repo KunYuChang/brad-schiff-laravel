187c3f31b9e3969006b48f343ff9e5b4fea2a940
KunYuChang <mischiefsub@gmail.com>
2024-02-28 12:49:49 +0800

    使用 Laravel 內建的身分驗證

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,40 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'You have successfully logged out.');
+    }
+
+    public function showCorrectHomepage()
+    {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        // Laravel 提供的 auth() 會去比對 User Model
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+
+            // 更新儲存在 cookie 的 session id
+            $request->session()->regenerate();
+            return redirect('/')->with('success', '登入成功。');
+        } else {
+            return redirect('/')->with('failure', '登入驗證失敗。');
+        }
+    }
+
+
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
@@ -18,7 +52,8 @@ class UserController extends Controller
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return 'Hello from register function';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', '感謝註冊帳號。');
     }
 }
